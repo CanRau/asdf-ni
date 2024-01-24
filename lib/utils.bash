c966f0f297742d97b7359fafaab228e2bd9388eb
CanRau <cansrau@gmail.com>
2024-01-24 08:14:49 -0500

    update logic

--- lib/utils.bash
+++ lib/utils.bash
@@ -4,6 +4,7 @@ set -euo pipefail
 
 # TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for ni.
 GH_REPO="https://github.com/antfu/ni"
+REGISTRY_URL="https://registry.npmjs.org/@antfu/ni"
 TOOL_NAME="ni"
 TOOL_TEST="ni --help"
 
@@ -24,16 +25,25 @@ sort_versions() {
 		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
-list_github_tags() {
-	git ls-remote --tags --refs "$GH_REPO" |
-		grep -o 'refs/tags/.*' | cut -d/ -f3- |
-		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
-}
+# list_github_tags() {
+# 	git ls-remote --tags --refs "$GH_REPO" |
+# 		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+# 		sed 's/^v//' # NOTE: You might want to adapt this sed to remove non-version strings from tags
+# }
 
 list_all_versions() {
-	# TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-	# Change this function if ni has other means of determining installable versions.
-	list_github_tags
+	curl --silent --fail --show-error --location ${REGISTRY_URL} |
+  grep -Eo '"version":"[^"]+"' |
+  cut -d\" -f4 |
+  sort_versions |
+  xargs echo
+}
+
+get_latest_version() {
+	curl --silent --fail --show-error --location ${REGISTRY_URL} |
+  grep -Eo '"latest":"[^"]+"' |
+  cut -d\" -f4 |
+  xargs echo
 }
 
 download_release() {
@@ -42,16 +52,23 @@ download_release() {
 	filename="$2"
 
 	# TODO: Adapt the release URL convention for ni
-	url="$GH_REPO/archive/v${version}.tar.gz"
+	# url="$GH_REPO/archive/v${version}.tar.gz"
+	# tarball url via npm view @antfu/ni dist.tarball
+	url="${REGISTRY_URL}/-/ni-${ASDF_INSTALL_VERSION}.tgz"
 
 	echo "* Downloading $TOOL_NAME release $version..."
+	# echo "\n\n$filename\n\n"
 	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	# tar xzf - --strip-components=1 --no-same-owner -C "$filename"
+	# echo "\n\nfilename\n\n"
+	# echo $(ls -lah $install_path)
+	# echo "\n\nfilename\n\n"
 }
 
 install_version() {
 	local install_type="$1"
 	local version="$2"
-	local install_path="${3%/bin}/bin"
+	local install_path="${3%/bin}"
 
 	if [ "$install_type" != "version" ]; then
 		fail "asdf-$TOOL_NAME supports release installs only"
@@ -59,11 +76,37 @@ install_version() {
 
 	(
 		mkdir -p "$install_path"
-		cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH/bin"/* "$install_path"
+
+		# chmod +x "$install_path/bin/*"
+		find "$install_path" -type f -exec chmod +x {} \;
+
+		# printf "\n\ninstall_path $install_path\n\n"
+		# echo $(ls -l $install_path)
+		for f in "$install_path"/*; do
+			# echo "File -> $f"
+			# ln -sf $(echo $f |sed -e 's/.mjs//') $f
+			mv $f $(echo $f |sed -e 's/.mjs//')
+		done
+
+		# printf "\n\n"
+		# find "$install_path" -type f -exec echo '{}' \;
+		# ls -l $install_path
+		# printf "\n\ninstall_path\n\n"
+
+		# printf "\n\nASDF_DOWNLOAD_PATH $ASDF_DOWNLOAD_PATH\n\n"
+		# # echo $(ls -l $ASDF_DOWNLOAD_PATH)
+		# for f in "$ASDF_DOWNLOAD_PATH/"; do
+		# 	echo "File -> $f"
+		# done
+		# printf "\n\nASDF_DOWNLOAD_PATH\n\n"
 
 		# TODO: Assert ni executable exists.
 		local tool_cmd
 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+
+		echo "\n\ntool_cmd $tool_cmd\n\n"
+
 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
 		echo "$TOOL_NAME $version installation was successful!"
@@ -72,3 +115,36 @@ install_version() {
 		fail "An error occurred while installing $TOOL_NAME $version."
 	)
 }
+
+# install_version() {
+# 	local install_type="$1"
+# 	local version="$2"
+# 	local install_path="$3"
+# 	# tarball url via npm view @antfu/ni dist.tarball
+# 	local tarball_url="https://registry.npmjs.org/@antfu/ni/-/ni-${ASDF_INSTALL_VERSION}.tgz"
+
+# 	if [ "$install_type" != "version" ]; then
+# 		fail "asdf-$TOOL_NAME supports release installs only"
+# 	fi
+
+# 	(
+# 		echo "Downloading pnpm v${version} from ${tarball_url}"
+# 		curl --silent --fail --show-error --location "${tarball_url}" |
+# 		tar xzf - --strip-components=1 --no-same-owner -C "${install_path}"
+# 		echo "\ninstall_path\n"
+# 		la $install_path
+# 		echo "\ninstall_path\n"
+# 		mkdir -p "$install_path"
+# 		# cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
+
+# 		# TODO: Assert ni executable exists.
+# 		local tool_cmd
+# 		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+# 		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+
+# 		echo "$TOOL_NAME $version installation was successful!"
+# 	) || (
+# 		rm -rf "$install_path"
+# 		fail "An error occurred while installing $TOOL_NAME $version."
+# 	)
+# }
